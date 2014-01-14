49b5f5ebde29831d70ff78f0b07db35839b098b6
Max Mays <cheeser12@ezen.com>
2014-01-14 10:45:51 -0500

    Table now requires a name to be valid

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,5 @@
 class Table < ActiveRecord::Base
   serialize :headers
   serialize :rows
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140109215847) do
+
+  create_table "tables", force: true do |t|
+    t.string   "name"
+    t.string   "headers"
+    t.string   "rows"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
