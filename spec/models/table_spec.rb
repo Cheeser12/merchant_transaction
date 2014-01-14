eacef43a97686710f6cc174aba6db03b2510bba4
Max Mays <cheeser12@ezen.com>
2014-01-14 10:46:11 -0500

    Added test for Table model

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Table do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    FactoryGirl.create(:table).should be_valid
+  end
+  it 'requires a name' do
+    FactoryGirl.build(:table, name: nil).should_not be_valid
+  end
 end
