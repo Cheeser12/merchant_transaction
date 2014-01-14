297210d852fcb0ae87b6b54920322ad987396999
Max Mays <cheeser12@ezen.com>
2014-01-14 11:22:55 -0500

    Refactored the styling of some specs

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe ReportsController do
   describe "GET #index" do
     it "lists all the tables" do
-      create(:table)
-      create(:table, name: "test2")
+      create :table
+      create :table, name: "test2"
       get :index  
       assigns[:table_ids].should eq Table.all.ids
     end
@@ -12,7 +12,7 @@ describe ReportsController do
 
   describe "GET #show" do
     it "gets the table" do
-      table = create(:table)
+      table = create :table
       get :show, id: table.id
       assigns[:table].should eq table
     end
--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Table do
   it 'has a valid factory' do
-    FactoryGirl.create(:table).should be_valid
+    (create :table).should be_valid
   end
   it 'requires a name' do
-    FactoryGirl.build(:table, name: nil).should_not be_valid
+    (build :table, name: nil).should_not be_valid
   end
 end
