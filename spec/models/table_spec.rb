8d7d4ccf36c68151e7d2309c10843cb31895f56b
Max Mays <cheeser12@ezen.com>
2014-01-14 13:48:24 -0500

    Removed some redundancy from table spec

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Table do
+  let(:table) { create :table }
+
   it 'has a valid factory' do
-    (create :table).should be_valid
+    table.should be_valid
   end
 
   it 'requires a name' do
@@ -11,7 +13,6 @@ describe Table do
 
   it 'exports to CSV' do
     require 'csv'
-    table = create :table
     csv = Table.to_csv(table)
     csv.to_s.should eq "header1,header2,header3\n1,2,3\n4,5,6\n7,8,9\n"
   end
